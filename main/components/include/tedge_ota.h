22722a6f20716bb17ccd59f521936614d022aeec
Reuben Miller <reuben.d.miller@gmail.com>
2024-03-23 01:35:47 +0100

    feat: add ota support

---
+++ main/components/include/tedge_ota.h
@@ -0,0 +1,7 @@
+
+#ifndef __TEDGE_OTA_H__
+#define __TEDGE_OTA_H__
+
+void advanced_ota_example_task(void *pvParameter);
+
+#endif
--- main/main.c
+++ main/main.c
@@ -32,9 +32,15 @@
 #include "tedge.h"
 #include "tedge_command.h"
 
+#include "esp_ota_ops.h"
+#include "esp_https_ota.h"
+#include "tedge_ota.h"
+
 #define STORAGE_NAMESPACE "storage"
 #define CONFIG_MQTT_HOST "mqtt_host"
 #define CONFIG_MQTT_PORT "mqtt_port"
+
+#define CONFIG_BOOTLOADER_APP_ROLLBACK_ENABLE
 
 static const char *TAG = "TEDGE";
 char APPLICATION_NAME[] = "freertos-esp32-tedge";
@@ -47,7 +53,7 @@ char APPLICATION_VERSION[] = "1.1.1";
 const char *MANUAL_MQTT_HOST = NULL;
 
 // Match the first service matching the given pattern
-const char *MDNS_DISCOVER_PATTERN = NULL;
+char *MDNS_DISCOVER_PATTERN = NULL;
 
 //
 // Persistence
@@ -110,6 +116,10 @@ int publish_mqtt_json(esp_mqtt_client_handle_t client, const char *topic, cJSON 
     free(payload);
     ESP_LOGD(TAG, "sent publish successful, msg_id=%d", msg_id);
     return msg_id;
+}
+
+void start_ota_update(void) {
+    // xTaskCreate(&advanced_ota_example_task, "advanced_ota_example_task", 1024 * 8, NULL, 5, NULL);
 }
 
 /*
@@ -608,6 +618,18 @@ static void mqtt_app_start(void)
 
 void app_main(void)
 {
+    // Initialize NVS.
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        // 1.OTA app partition table has a smaller NVS partition size than the non-OTA
+        // partition table. This size mismatch may cause NVS initialization to fail.
+        // 2.NVS partition contains data in new format and cannot be recognized by this version of code.
+        // If this happens, we erase NVS partition and initialize NVS again.
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK( err );
+
     ESP_LOGI(TAG, "Startup..");
     ESP_LOGI(TAG, "Free memory: %d bytes", esp_get_free_heap_size());
     ESP_LOGI(TAG, "IDF version: %s", esp_get_idf_version());
@@ -632,4 +654,23 @@ void app_main(void)
 
     ESP_ERROR_CHECK(example_connect());
     mqtt_app_start();
+
+#if defined(CONFIG_BOOTLOADER_APP_ROLLBACK_ENABLE)
+    /**
+     * We are treating successful WiFi connection as a checkpoint to cancel rollback
+     * process and mark newly updated firmware image as active. For production cases,
+     * please tune the checkpoint behavior per end application requirement.
+     */
+    const esp_partition_t *running = esp_ota_get_running_partition();
+    esp_ota_img_states_t ota_state;
+    if (esp_ota_get_state_partition(running, &ota_state) == ESP_OK) {
+        if (ota_state == ESP_OTA_IMG_PENDING_VERIFY) {
+            if (esp_ota_mark_app_valid_cancel_rollback() == ESP_OK) {
+                ESP_LOGI(TAG, "App is valid, rollback cancelled successfully");
+            } else {
+                ESP_LOGE(TAG, "Failed to cancel rollback");
+            }
+        }
+    }
+#endif
 }
