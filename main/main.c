2d383c267f72a5527ed6850ecc90de5fff860c41
Yishu Zhong <yiz@softwareag.com>
2024-04-10 16:11:07 +0200

    Save wifi credentials in nvs

--- main/main.c
+++ main/main.c
@@ -488,6 +488,49 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
     mqtt_event_handler_cb(event_data);
 }
 
+int retry_num=0;
+static void wifi_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id,void *event_data){
+if(event_id == WIFI_EVENT_STA_START)
+{
+  printf("WIFI CONNECTING....\n");
+}
+else if (event_id == WIFI_EVENT_STA_CONNECTED)
+{
+  printf("WiFi CONNECTED\n");
+}
+else if (event_id == WIFI_EVENT_STA_DISCONNECTED)
+{
+  printf("WiFi lost connection\n");
+  if(retry_num<5){esp_wifi_connect();retry_num++;printf("Retrying to Connect...\n");}
+}
+else if (event_id == IP_EVENT_STA_GOT_IP)
+{
+  printf("Wifi got IP...\n\n");
+}
+}
+
+void wifi_connection(){
+    esp_netif_init(); //network interdace initialization
+    esp_event_loop_create_default(); //responsible for handling and dispatching events
+    esp_netif_create_default_wifi_sta(); //sets up necessary data structs for wifi station interface
+    wifi_init_config_t wifi_initiation = WIFI_INIT_CONFIG_DEFAULT();//sets up wifi wifi_init_config struct with default values
+    esp_wifi_init(&wifi_initiation); //wifi initialised with dafault wifi_initiation
+    esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, wifi_event_handler, NULL);//creating event handler register for wifi
+    esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, wifi_event_handler, NULL);//creating event handler register for ip event
+    wifi_config_t wifi_configuration;
+    esp_wifi_get_config(ESP_IF_WIFI_STA, &wifi_configuration);
+
+    if(strlen(&wifi_configuration.sta.ssid) == 0 || strlen(&wifi_configuration.sta.password) == 0) {
+        strcpy((char*)wifi_configuration.sta.ssid, CONFIG_EXAMPLE_WIFI_SSID); // copy chars from hardcoded configs to struct
+        strcpy((char*)wifi_configuration.sta.password, CONFIG_EXAMPLE_WIFI_PASSWORD);
+        esp_wifi_set_storage(WIFI_STORAGE_FLASH);
+    } 
+    esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_configuration);//setting up configs when event ESP_IF_WIFI_STA
+    esp_wifi_start();//start connection with configurations provided in funtion
+    esp_wifi_set_mode(WIFI_MODE_STA);//station mode selected
+    esp_wifi_connect(); //connect with saved ssid and pass
+}   
+
 static void mqtt_app_start(void)
 {
     // Use mdns to discover which local thin-edge.io to connect to
@@ -810,6 +853,9 @@ void app_main(void)
      */
     tedge_banner();
 
-    ESP_ERROR_CHECK(example_connect());
+    // ESP_ERROR_CHECK(example_connect());
+    nvs_flash_init();
+    wifi_connection();
+
     mqtt_app_start();
 }
